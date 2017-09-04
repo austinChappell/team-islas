1ec163833242683307445f13b6fa1c255ad70c2e
austinChappell <Austin.W.Chappell@gmail.com>
2017-09-04 16:10:57 -0500

    js for desktop only

--- super-hands-script.js
+++ super-hands-script.js
@@ -3,16 +3,18 @@ const quoteArray = document.querySelectorAll('.quote-container');
 const quoteSectionArray = document.querySelectorAll('.quote');
 
 quoteSectionArray.forEach((section) => {
-  section.style.opacity = '0';
-  let midOfElem = section.offsetTop + section.offsetHeight / 2;
-  console.log(section.children[1]);
-  window.addEventListener('scroll', () => {
-    let windowBottom = window.scrollY + window.outerHeight;
-    if (windowBottom > midOfElem) {
-      section.style.opacity = '1';
-      section.children[0].style.left = 0;
-      // margin-left is for mobile
-      section.children[0].style.marginLeft = 0;
-    }
-  })
+  if (window.innerWidth > 800) {    
+    section.style.opacity = '0';
+    let midOfElem = section.offsetTop + section.offsetHeight / 2;
+    // console.log(section.clientHeight);
+    window.addEventListener('scroll', () => {
+      let windowBottom = window.scrollY + window.innerHeight;
+      if (windowBottom > midOfElem) {
+        section.style.opacity = '1';
+        section.children[0].style.left = 0;
+        // margin-left is for mobile
+        // section.children[0].style.marginLeft = 0;
+      }
+    })
+  }
 })
