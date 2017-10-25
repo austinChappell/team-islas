b18eeb8b23f597c76124de9a205f66eeb1a2f005
austinChappell <Austin.W.Chappell@gmail.com>
2017-10-25 05:39:30 -0500

    add videos to super hands page

--- super-hands-script.js
+++ super-hands-script.js
@@ -3,7 +3,7 @@ const quoteArray = document.querySelectorAll('.quote-container');
 const quoteSectionArray = document.querySelectorAll('.quote');
 
 quoteSectionArray.forEach((section) => {
-  if (window.innerWidth > 800) {    
+  if (window.innerWidth > 800) {
     section.style.opacity = '0';
     let midOfElem = section.offsetTop + section.offsetHeight / 2;
     // console.log(section.clientHeight);
@@ -14,6 +14,13 @@ quoteSectionArray.forEach((section) => {
         section.children[0].style.left = 0;
         // margin-left is for mobile
         // section.children[0].style.marginLeft = 0;
+      }
+    })
+  } else {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 0) {
+        section.style.opacity = '1';
+        section.children[0].style.left = 0;        
       }
     })
   }
